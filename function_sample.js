c30e7bbd8cac426cddb3d4fcad4edd80f3ac8003
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-22 16:28:20 +0300

    Javascript functions

---
+++ function_sample.js
@@ -0,0 +1,15 @@
+/**
+ * Cytonn Technologies
+ *
+ * @author: Ronnie Nyaga <rnyaga@cytonn.com>
+ *
+ * Project: js.app
+ *
+ */
+
+
+
+// Function that takes length and width, and returns the area of a rectangle
+function areaOfRectangle(length,width){
+	return length * width;
+}
